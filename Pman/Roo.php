9bee2d2e3440351650297b1665f786cabdae32ff
Alan Knowles <alan@akbkhome.com>
2010-06-14 16:25:41 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -61,7 +61,7 @@ class Pman_Roo extends Pman
     function get($tab)
     {
          //  $this->jerr("Not authenticated", array('authFailure' => true));
-        //DB_DataObject::debuglevel(1);
+         DB_DataObject::debuglevel(1);
         
         
         if (!empty($_GET['_post'])) {

861e0a12c5ed553ce053502bca6287a64597aa78
Alan Knowles <alan@akbkhome.com>
2010-07-19 18:33:02 +0800

    Pman/I18N.php

--- Pman/I18N.php
+++ Pman/I18N.php
@@ -364,7 +364,7 @@ class Pman_I18N extends Pman
             return;
         }
         
-        $list = $ltype == 'c' ? $this->getDefaultCfg($ltype) : $this->cfg[$ltype];
+        $list =  $this->getDefaultCfg($ltype);
         
         DB_DataObject::debugLevel(1);
         

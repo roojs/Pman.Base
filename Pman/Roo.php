209e77fb7f78d5138eef6d2778f40e4c66b4466f
Alan Knowles <alan@akbkhome.com>
2010-07-23 12:17:23 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -482,7 +482,7 @@ class Pman_Roo extends Pman
             if ($filter) {
                 $cols = array();
                 foreach($xx as $c) {
-                    $tn = sprintf($ocl.'_%s', $table, $c, $c);
+                    $tn = sprintf($ocl.'_%s', 'join_'.$ocl.'_'. $col, $c, $c);
                     if (in_array($tn, $filter)) {
                         $cols[] = $c;
                     }

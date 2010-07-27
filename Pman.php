35ef766bd476a0e50cab1f7a98bcb41f82a91b28
Alan Knowles <alan@akbkhome.com>
2010-07-27 13:55:05 +0800

    Pman.php

--- Pman.php
+++ Pman.php
@@ -557,7 +557,9 @@ class Pman extends HTML_FlexyFramework_Page
         if (file_exists($cfile)) {
            // $ctime = max(filemtime($cfile), filectime($cfile));
             // otherwise use compile dfile..
-            $ar = glob($cfile / '/' . $mod + '*.js');
+            $cfile = array_pop(glob($cfile / '/' . $mod + '*.js'));
+            echo $cfile;exit;
+            
             $files = array( $this->rootURL."/_compiled_/". basename($cfile));
             if (file_exists($lfile)) {
                 array_push($files, $this->rootURL."/_translations_/$mod.js");

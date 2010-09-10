4b27e7d222c39dd052a74aedfcb9e9779a914554
Alan Knowles <alan@akbkhome.com>
2010-09-10 13:22:00 +0800

    Pman/GnumericToExcel.php

--- Pman/GnumericToExcel.php
+++ Pman/GnumericToExcel.php
@@ -78,7 +78,8 @@ class Pman_GnumericToExcel extends Pman
         
         $fh = fopen($targetTmp, 'r');
         header('Content-type: application/vnd.ms-excel');
-         header('Content-Disposition: attachment; filename="' .addslashes($fname). '"');
+        header('Content-Disposition: attachment; filename="' .addslashes($fname). '"');
+         
         // will not work on IE... - needs while/fget..
         fpassthru($fh);
         unlink($targetTmp);

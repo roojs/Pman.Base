b543a2e33fdfcc68db337f8d7f5952b81880ef40
Alan Knowles <alan@akbkhome.com>
2010-09-21 13:23:29 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -175,31 +175,7 @@ class Pman_Roo extends Pman
         // sorting..
       //   DB_DataObject::debugLevel(1);
         
-        $sort = empty($_REQUEST['sort']) ? '' : $_REQUEST['sort'];
-        $dir = (empty($_REQUEST['dir']) || strtoupper($_REQUEST['dir']) == 'ASC' ? 'ASC' : 'DESC');
-        
-        $sorted = false;
-        if (method_exists($x, 'applySort')) {
-            $sorted = $x->applySort($this->authUser, $sort, $dir, array_keys($this->cols));
-        }
-        if ($sorted === false) {
-            
-            $cols = $x->table();
-           // echo '<PRE>';print_r(array($sort, $this->cols));
-            // other sorts??? 
-           // $otherSorts = array('person_id_name');
-            
-            if (strlen($sort) && isset($cols[$sort]) ) {
-                $sort = $x->tableName() .'.'.$sort . ' ' . $dir ;
-                $x->orderBy($sort );
-            } else if (in_array($sort, array_keys($this->cols))) {
-                $sort = $sort . ' ' . $dir ;
-                $x->orderBy($sort );
-            }// else other formatas?
-            //if ( in_array($sort, $otherSorts)) {
-            //    $x->orderBy($sort . ' ' . $dir);
-            ////}
-        }
+        $this->applySort($x);
         
         
  
@@ -283,6 +259,48 @@ class Pman_Roo extends Pman
         $this->jdata($ret,$total, $extra );
 
     
+    }
+    /**
+     * applySort
+     * 
+     * apply REQUEST[sort] and [dir]
+     * sort may be an array of columsn..
+     * 
+     * @arg   DB_DataObject $x
+     * 
+     */
+    function applySort($x)
+    {
+        
+    
+        $sort = empty($_REQUEST['sort']) ? '' : $_REQUEST['sort'];
+        $dir = (empty($_REQUEST['dir']) || strtoupper($_REQUEST['dir']) == 'ASC' ? 'ASC' : 'DESC');
+        
+        
+        
+        $sorted = false;
+        if (method_exists($x, 'applySort')) {
+            $sorted = $x->applySort($this->authUser, $sort, $dir, array_keys($this->cols));
+        }
+        if ($sorted === false) {
+            
+            $cols = $x->table();
+            $sort = explode(',', $sort);
+           // echo '<PRE>';print_r(array($sort, $this->cols));
+            // other sorts??? 
+           // $otherSorts = array('person_id_name');
+            
+            if (strlen($sort) && isset($cols[$sort]) ) {
+                $sort = $x->tableName() .'.'.$sort . ' ' . $dir ;
+                $x->orderBy($sort );
+            } else if (in_array($sort, array_keys($this->cols))) {
+                $sort = $sort . ' ' . $dir ;
+                $x->orderBy($sort );
+            }// else other formatas?
+            //if ( in_array($sort, $otherSorts)) {
+            //    $x->orderBy($sort . ' ' . $dir);
+            ////}
+        }
     }
      /**
      * POST method   Roo/TABLENAME.php 

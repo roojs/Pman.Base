6bc273444632af4754d0f330c1dbcd187347d427
Alan Knowles <alan@akbkhome.com>
2010-12-21 10:52:19 +0800

    Pman/Login.php

--- Pman/Login.php
+++ Pman/Login.php
@@ -201,7 +201,7 @@ class Pman_Login extends Pman
         }
         
         // check if config allows non-owner passwords.
-        // auth_company = "OWNER"
+        // auth_company = "OWNER" auth_company = "CLIENT"
         $ff= HTML_FlexyFramework::get();
         if (!empty($ff->Pman['auth_comptype']) && $ff->Pman['auth_comptype'] != $u->company()->comptype) {
             $this->jerr("Login not permited to outside companies");

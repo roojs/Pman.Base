258398ad1aa6378b218bd07253f0283ac3a11910
Alan Knowles <alan@akbkhome.com>
2012-01-16 12:56:08 +0800

    Pman.php

--- Pman.php
+++ Pman.php
@@ -506,7 +506,10 @@ class Pman extends HTML_FlexyFramework_Page
     }
     function jok($str)
     {
-        
+         if ($cli) {
+            echo "OK: " .$str . "\n";
+            exit;
+        }
         require_once 'Services/JSON.php';
         $json = new Services_JSON();
         

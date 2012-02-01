b6bd91f62f1ee15f1edc67a1d0b737fab58e170b
Alan Knowles <alan@akbkhome.com>
2012-02-01 22:18:10 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -179,8 +179,7 @@ class Pman_Roo extends Pman
         
         // single fetch (use '0' to fetch an empty object..)
         if (isset($_REQUEST['_id']) && is_numeric($_REQUEST['_id'])) {
-            
-            die("calling single select");
+             
              $this->selectSingle($x, $_REQUEST['_id'],$_REQUEST);
              // actually exits.
         }

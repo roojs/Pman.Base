c3de8530de65dfb2cce2ed25f777a26a7f517b32
Alan Knowles <alan@akbkhome.com>
2012-05-03 16:25:43 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -247,7 +247,7 @@ class Pman_Roo extends Pman
  
         $x->limit(
             empty($_REQUEST['start']) ? 0 : (int)$_REQUEST['start'],
-            min(empty($_REQUEST['limit']) ? 25 : (int)$_REQUEST['limit'], 5000)
+            min(empty($_REQUEST['limit']) ? 25 : (int)$_REQUEST['limit'], 10000)
         );
         
         $queryObj = clone($x);

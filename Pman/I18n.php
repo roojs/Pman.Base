2fa2626a869794f45344f5c80721a30b5781cf55
Chris <chris@roojs.com>
2012-06-20 17:12:52 +0800

    MOVED Pman/I18N.php to Pman/I18n.php

---
+++ Pman/I18n.php
@@ -0,0 +1,43 @@
+<?php
+
+/// provide language data!!!
+// DEPRECIATED - moved to Pman_Core_I18N
+/**
+ * 
+ * 
+ */
+
+require_once 'Pman/Core/I18n.php';
+class Pman_I18N extends Pman_Core_I18n
+{
+     
+    
+    
+     
+    /*
+    
+    function setSession($au)
+    {
+        $this->authUser = $au;
+        $lbits = implode('_', $this->guessUsersLanguage());
+        if (empty($_SESSION['Pman_I18N'])) {
+            $_SESSION['Pman_I18N']  = array();
+        }
+        
+        $_SESSION['Pman_I18N'][$lbits] = array(
+            'l' => $this->getList('l', $lbits),
+            'c' => $this->getList('c', $lbits),
+            'm' => $this->getList('m', $lbits),
+        );
+        
+        
+    }
+      
+   
+     
+   
+   
+    */
+     
+    
+}

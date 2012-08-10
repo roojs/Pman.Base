72b8162882bb09689767088668103e226e1d4c87
Chris <chris@roojs.com>
2012-08-10 19:09:21 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -789,7 +789,7 @@ class Pman_Roo extends Pman
     }
     
     
-    function update($x, $req,  $with_perm_check)
+    function update($x, $req,  $with_perm_check = true)
     {
         if ( $with_perm_check && method_exists($x, 'checkPerm') && !$x->checkPerm('E', $this->authUser, $_REQUEST))  {
             $this->jerr("PERMISSION DENIED - No Edit permissions on this element");

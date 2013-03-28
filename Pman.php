8bd3688b038c35fc131a60d54c8df8bdc8990da5
Alan Knowles <alan@roojs.com>
2013-03-28 11:00:11 +0800

    Pman.php

--- Pman.php
+++ Pman.php
@@ -774,6 +774,50 @@ class Pman extends HTML_FlexyFramework_Page
         
     }
     
+    /**
+     * Error handling...
+     * 
+     */
+    
+    static $permitError = false;
+    
+    function onPearError($err)
+    {
+        static $reported = false;
+        if ($reported) {
+            return;
+        }
+        
+        if (Pman_Roo::$permitError) {
+             
+            return;
+            
+        }
+        
+        
+        $reported = true;
+        $out = $err->toString();
+        
+        
+        //print_R($bt); exit;
+        $ret = array();
+        $n = 0;
+        foreach($err->backtrace as $b) {
+            $ret[] = @$b['file'] . '(' . @$b['line'] . ')@' .   @$b['class'] . '::' . @$b['function'];
+            if ($n > 20) {
+                break;
+            }
+            $n++;
+        }
+        //convert the huge backtrace into something that is readable..
+        $out .= "\n" . implode("\n",  $ret);
+     
+        
+        $this->jerr($out);
+        
+        
+        
+    }
     
     
     /**

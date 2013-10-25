fb91b89a16b01c6ba9b7908e2f631466cf39b0d9
Edward <edward@roojs.com>
2013-10-25 14:19:57 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -389,6 +389,25 @@ class Pman_Roo extends Pman
         
     }
     
+    function checkDebugPost()
+    {
+         if (isset($_REQUEST['_debug']) 
+                && 
+                $this->authUser
+                &&
+                method_exists($this->authUser,'groups') 
+                &&
+                is_a($this->authUser, 'Pman_Core_DataObjects_Person')
+                &&
+                in_array('Administrators', $this->authUser->groups('name'))
+                
+            ){
+            DB_DAtaObject::debuglevel((int)$_REQUEST['_debug']);
+        }
+        
+    }
+    
+    
     function toCsv($data, $cols, $titles, $filename, $addDate = true)
     {
           

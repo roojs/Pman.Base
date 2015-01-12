aa8b273ecdf77f488bef33f6858a4a33549d5b08
Alan Knowles <alan@roojs.com>
2015-01-12 11:10:04 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -380,14 +380,12 @@ class Pman_Roo extends Pman
          if (isset($_REQUEST['_debug']) 
                 && 
                 $this->authUser
-                /*
                 &&
                 method_exists($this->authUser,'groups') 
                 &&
                 is_a($this->authUser, 'Pman_Core_DataObjects_Person')
                 &&
                 in_array('Administrators', $this->authUser->groups('name'))
-                */
                 
             ){
             DB_DAtaObject::debuglevel((int)$_REQUEST['_debug']);

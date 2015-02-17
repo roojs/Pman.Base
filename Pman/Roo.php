547aba86e07aefaccd3b0a0d810cb9ae71eefbac
Michael <michael@roojs.com>
2015-02-17 10:37:06 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -810,7 +810,6 @@ class Pman_Roo extends Pman
             
             
             if (method_exists($x, 'toRooSingleArray')) {
-                $this->jok('here');
                 $this->jok($x->toRooSingleArray($this->authUser, $req));
             }
             if (method_exists($x, 'toRooArray')) {

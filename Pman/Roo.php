3bf508c0646c4a0a2bc41a8bd04b3e9f85af9b45
Michael <michael@roojs.com>
2015-02-17 10:49:58 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -803,7 +803,7 @@ class Pman_Roo extends Pman
     function selectSingle($x, $id, $req=false)
     {
          
-        print_r($x);exit;
+        print_r($id);exit;
         $_columns = !empty($req['_columns']) ? explode(',', $req['_columns']) : false;
         //var_dump(array(!is_array($id) , empty($id)));
         if (!is_array($id) && empty($id)) {

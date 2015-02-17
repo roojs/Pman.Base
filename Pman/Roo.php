deac5f75fd78bb0fe1ecce455b896275a2376d92
Michael <michael@roojs.com>
2015-02-17 11:00:48 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -845,7 +845,7 @@ class Pman_Roo extends Pman
         } else if (!$x->get($id)) {
             $this->jerr("selectSingle: no such record ($id)");
         }
-        print_r($x);exit;
+        
         // ignore perms if comming from update/insert - as it's already done...
         if ($req !== false && method_exists($x, 'checkPerm') && !$x->checkPerm('S', $this->authUser))  {
             $this->jerr("PERMISSION DENIED - si");

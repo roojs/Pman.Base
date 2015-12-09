85372fff94aee4c8f2ce01f8dae42bc510fb6f2d
Alan Knowles <alan@roojs.com>
2015-12-09 15:04:45 +0800

    Pman.php

--- Pman.php
+++ Pman.php
@@ -457,6 +457,8 @@ class Pman extends HTML_FlexyFramework_Page
     }
     /**
      * Recomended JSON error indicator
+     *
+     * 
      * @param string $type  - normally 'ERROR' - you can use this to track error types.
      * @param string $message - error message displayed to user.
      * @param array $errors - optioanl data to pass to front end.

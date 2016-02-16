ff24b3e553041a22712170b7ebbd522d79052635
edward <edward@roojs.com>
2016-02-16 15:38:19 +0800

    Pman.php

--- Pman.php
+++ Pman.php
@@ -504,10 +504,19 @@ class Pman extends HTML_FlexyFramework_Page
         
         // log all errors!!!
         
+        $retHTML = isset($_SERVER['CONTENT_TYPE']) && 
+                preg_match('#multipart/form-data#i', $_SERVER['CONTENT_TYPE']);
         
-        if (!empty($_REQUEST['returnHTML']) || 
-            (isset($_SERVER['CONTENT_TYPE']) && preg_match('#multipart/form-data#i', $_SERVER['CONTENT_TYPE']))
-        ) {
+        if ($retHTML){
+            if (isset($_REQUEST['returnHTML']) && $_REQUEST['returnHTML'] == 'NO') {
+                $retHTML = false;
+            }
+        } else {
+            $retHTML = isset($_REQUEST['returnHTML']) && $_REQUEST['returnHTML'] !='NO';
+        }
+        
+        
+        if ($retHTML) {
             header('Content-type: text/html');
             echo "<HTML><HEAD></HEAD><BODY>";
             echo  $json->encodeUnsafe(array(

462a2b59ca53e819f9666d23d157233b180bf228
Alan Knowles <alan@roojs.com>
2016-03-16 11:05:11 +0800

    Pman.php

--- Pman.php
+++ Pman.php
@@ -690,11 +690,13 @@ class Pman extends HTML_FlexyFramework_Page
      */
     function outputJavascriptIncludes()  
     {
-         
+        // BC support - currently 1 project still relies on this.. (MO portal) 
         $o = HTML_FlexyFramework::get()->Pman_Core;
         if (isset($o['packseed'])) {
             return $this->outputJavascriptIncludesBC();
         }
+        
+        
         $mods = $this->modulesList();
         
         foreach($mods as $mod) {

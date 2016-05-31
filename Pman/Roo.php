7f4aaa73644b649671169973c270c9388ccee93c
edward <edward@roojs.com>
2016-05-31 11:02:23 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -372,6 +372,7 @@ class Pman_Roo extends Pman
         // this make take some time...
         $this->sessionState(0);
        // echo "<PRE>"; print_r($ret);
+        print_R($ret);exit;
         $this->jdata($ret, max(count($ret), $total), $extra );
 
     

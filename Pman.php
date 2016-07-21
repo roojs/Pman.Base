2d5d0a726eff2bd77692c936395b349bf23f36f9
Alan Knowles <alan@roojs.com>
2016-07-21 11:48:03 +0800

    Pman.php

--- Pman.php
+++ Pman.php
@@ -494,7 +494,7 @@ class Pman extends HTML_FlexyFramework_Page
         $cli = HTML_FlexyFramework::get()->cli;
         if ($cli) {
             echo "ERROR: " .$str . "\n";
-            exit(1);
+            exit(1); // cli --- exit code to stop shell execution if necessary.
         }
         
         

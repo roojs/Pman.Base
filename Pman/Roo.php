2d7d99c205aadf2f1d011b7125768d6115cdfd4a
edward <edward@roojs.com>
2016-08-12 13:44:19 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -58,13 +58,6 @@ class Pman_Roo extends Pman
     
     function getAuth()
     {
-        if (!isset($_SERVER['PHP_AUTH_USER'])) {
-    header('WWW-Authenticate: Basic realm="My Realm"');
-    header('HTTP/1.0 401 Unauthorized');
-    echo 'Text to send if user hits Cancel button';
-    exit;
-}
-
         parent::getAuth(); // load company!
         $au = $this->getAuthUser();
        

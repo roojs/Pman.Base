a56074516e5b4160bb9be5ef5df4e8e06f275366
edward <edward@roojs.com>
2016-09-05 13:38:09 +0800

    Pman/Login.php

--- Pman/Login.php
+++ Pman/Login.php
@@ -41,7 +41,7 @@ class Pman_Login extends Pman
     function get() 
     {
         
-         print_R($_GET);exit;
+         
          //DB_DataObject::DebugLevel(1);
         if (!empty($_REQUEST['logout'])) {
             $u = $this->getAuthUser();

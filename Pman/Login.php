e27d59357376affe872a5997704a0541976fd3cc
edward <edward@roojs.com>
2016-09-05 15:53:16 +0800

    Pman/Login.php

--- Pman/Login.php
+++ Pman/Login.php
@@ -40,8 +40,7 @@ class Pman_Login extends Pman
      */
     function get() 
     {
-        $this->sendAuthUserDetails();
-            exit;
+        
          
          //DB_DataObject::DebugLevel(1);
         if (!empty($_REQUEST['logout'])) {

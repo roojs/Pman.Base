eb94c5fe9aade8e5929e15ea701e0bcbc30ff5c8
edward <edward@roojs.com>
2016-12-15 09:49:44 +0800

    Pman/Login.php

--- Pman/Login.php
+++ Pman/Login.php
@@ -214,6 +214,7 @@ class Pman_Login extends Pman
     var $domObj = false;
     function post()
     {
+        print_R('in??');exit;
         
         //DB_DataObject::debugLevel(1);
         if (!empty($_REQUEST['getAuthUser'])) {

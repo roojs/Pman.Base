cdbac3e191be0415c78006cf780eded96344a447
Alan Knowles <alan@roojs.com>
2017-05-24 10:47:31 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -672,7 +672,7 @@ class Pman_Roo extends Pman
         if (!empty($_REQUEST[$this->key])) {
             // it's a create..
             if (!$x->get($this->key, $_REQUEST[$this->key]))  {
-                $this->jerr("Invalid request (id not set to valid id)");
+                $this->jerr("Invalid request (id does not point to  a record.)");
             }
             $this->jok($this->update($x, $_REQUEST));
         } else {

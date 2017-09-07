88b5b349444b08b2db8c061ee6c06a623d4c4a22
edward <edward@roojs.com>
2017-09-07 16:50:15 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -61,8 +61,7 @@ class Pman_Roo extends Pman
     {
         parent::getAuth(); // load company!
         $au = $this->getAuthUser();
-        print_R($au->pid());exit;
-        
+       
         if (!$au) {  
             $this->jerr("Not authenticated", array('authFailure' => true));
         }

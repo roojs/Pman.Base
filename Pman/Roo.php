0054da9971af0f4a072df79c1e69b57a699de844
edward <edward@roojs.com>
2017-09-14 18:30:35 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -630,8 +630,7 @@ class Pman_Roo extends Pman
         //        $this->jerr("PERMISSION DENIED (e)");
         //}
         $this->initErrorHandling();
-        $this->jerr($tab);
-        print_r($tab);exit;
+        
         // DB_DataObject::debugLevel(1);
         $this->checkDebug();
         

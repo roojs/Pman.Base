b929e175d9b0a940683c7ba75356b6be3470ba6b
edward <edward@roojs.com>
2017-09-14 17:40:09 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -655,6 +655,7 @@ class Pman_Roo extends Pman
         
           // delete should be here...
         if (isset($_REQUEST['_delete'])) {
+            print_R($_REQUEST['_delete']);exit;
             // do we really delete stuff!?!?!?
             return $this->delete($x,$_REQUEST);
         } 

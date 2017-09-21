e1afc23d6c1656813c2d526a9dd19608d5bc2288
Alan Knowles <alan@roojs.com>
2017-09-21 16:29:27 +0800

    sync

--- Pman.php
+++ Pman.php
@@ -193,7 +193,7 @@ class Pman extends HTML_FlexyFramework_Page
         if ($this->company && $this->company->logo_id) {
             $im = DB_DataObject::Factory('Images');
             $im->get($this->company->logo_id);
-            $this->appLogo = $this->baseURL . '/Images/Thumb/300x100/'. $this->company->logo_id .'/' . $im->filename;
+            $this->appLogo = $this->baseURL . '/Images/Thumb/x100/'. $this->company->logo_id .'/' . $im->filename;
         }
         
         header('Content-type: text/html; charset=utf-8');

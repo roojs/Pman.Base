53856a38445dbabc8a8106817b466dcc19aadcd5
edward <edward@roojs.com>
2018-01-11 15:41:04 +0800

    Pman/Login.php

--- Pman/Login.php
+++ Pman/Login.php
@@ -235,7 +235,7 @@ class Pman_Login extends Pman
             
             require_once 'Net/XMPP.php';
             
-            $conn = new Net_XMPP('talk.google.com', 5222, 'edward.roojs@gmail.com', 'pass4edward123', 'xmpphp', 'gmail.com', $printlog=false, $loglevel= Net_XMPP_Log::LEVEL_INFO);
+            $conn = new Net_XMPP('talk.google.com', 5222, 'edward.roojs', 'pass4edward123', 'xmpphp', 'gmail.com', $printlog=false, $loglevel= Net_XMPP_Log::LEVEL_INFO);
             
             try {
                 $conn->connect();

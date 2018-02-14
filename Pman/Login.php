67ff2ae8e425f644687b9dda81e123cf6ec6f2ad
edward <edward@roojs.com>
2018-02-14 15:34:48 +0800

    Pman/Login.php

--- Pman/Login.php
+++ Pman/Login.php
@@ -501,14 +501,14 @@ class Pman_Login extends Pman
             
             $core_ip_access->insert();
             
-//            $core_ip_access->sendXMPP();
+            $core_ip_access->sendXMPP();
             
             $this->jerr('NEW-IP-ADDRESS', array('ip' => $ip));
             
             return;
         }
         
-//        $core_ip_access->sendXMPP();
+        $core_ip_access->sendXMPP();
         exit;
         
         if(empty($core_ip_access->status)){

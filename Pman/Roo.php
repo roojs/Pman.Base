6cc985eef4c00222f9a33212f1032bb30dd039d1
benny <benny@roojs.com>
2018-08-22 15:53:10 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -688,7 +688,7 @@ class Pman_Roo extends Pman
             if (empty($_POST)) {
                 $this->jerr("No data recieved for inserting");
             }
-
+            echo "here"; exit;
             $this->jok($this->insert($x, $_REQUEST));
             
         }

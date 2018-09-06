4e576601e0e2745ceea85026f34aaa159aca0cbb
benny <benny@roojs.com>
2018-09-06 11:17:04 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -1501,7 +1501,7 @@ class Pman_Roo extends Pman
                     
                     continue;
             }
-        }
+        }var_dump($q_filtered); die;
         if (!empty($q_filtered)) {
             //var_dump($q_filtered);
             

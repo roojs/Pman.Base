7e6830888964a6c3c23799515c535b6669ed6bc1
john <john@roojs.com>
2018-10-11 11:39:25 +0800

    Pman/Login.php

--- Pman/Login.php
+++ Pman/Login.php
@@ -133,7 +133,7 @@ class Pman_Login extends Pman
         if (!$u->isAuth()) {
             $this->jok(array(
                 'id' => 0,
-                 'disable_oath' => bool() $s->lookup('core', 'two_factor_authentication') ? 0 : 1
+                 'disable_oath' => (bool) $s->lookup('core', 'two_factor_authentication') ? 0 : 1
              )); // not logged in..
              exit;
         }

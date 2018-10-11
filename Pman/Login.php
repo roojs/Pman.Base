b00f4cb8d19368e2fa846812452599eea7dfdcae
john <john@roojs.com>
2018-10-11 13:50:14 +0800

    Pman/Login.php

--- Pman/Login.php
+++ Pman/Login.php
@@ -130,10 +130,10 @@ class Pman_Login extends Pman
         
         $u = DB_DataObject::factory($tbl);
         $s = DB_DataObject::factory('core_setting');
-        $oath_require_val = 1;
-        $oath_require = $s->lookup('core', 'two_factor_authentication_requirement');
-        if(!empty($oath_require)) {
-            if($oath_require->val == 0) {
+        $require_oath = 1;
+        $require_oath = $s->lookup('core', 'two_factor_authentication_requirement');
+        if(!empty($require_oath)) {
+            if($require_oath->val == 0) {
                 $oath_require_val = 0;
             }
         } 

18aa4b498f11b216b10ac8cf91590269944f0144
leon <leon@roojs.com>
2022-08-02 12:27:08 +0800

    Changed Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -881,6 +881,7 @@ class Pman_Roo extends Pman
             $x->applyFilters(array('_is_update_request' => true), $this->authUser, $this);
         }
         
+        die("AAA");
         // DB_DataObject::DebugLevel(1);
         if (is_array($id)) {
             // lookup...

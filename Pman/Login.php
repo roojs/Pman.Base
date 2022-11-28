aae1a2b7069779b2891eea5862dbe9324d19db19
Alan <alan@roojs.com>
2022-11-28 12:18:00 +0800

    ws

--- Pman/Login.php
+++ Pman/Login.php
@@ -88,7 +88,7 @@ class Pman_Login extends Pman
     {
         $ff = class_exists('HTML_FlexyFramework2') ?  HTML_FlexyFramework2::get()  :  HTML_FlexyFramework::get();
         
-	//DB_DAtaObject::debugLevel(1);
+		//DB_DAtaObject::debugLevel(1);
         $u = $this->getAuthUser();
         //print_r($u);
         if ($u) {

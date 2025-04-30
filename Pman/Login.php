8fe17b87e0814097bcf8df23cf631efd6a63338e
Alan <alan@roojs.com>
2025-04-30 11:09:32 +0800

    disable login url

--- Pman/Login.php
+++ Pman/Login.php
@@ -9,21 +9,24 @@ class Pman_Login extends Pman {
 
     function get($base, $o= array())
     {
+        $this->jnotice("INVALIDURL","Invalid request");
+        /*
         require_once 'Pman/Core/Auth/State.php';
         $n = new Pman_Core_Auth_State();
         $n->post($base, $o);
         
-        
+        */
         
     }
     
     function post($base, $o = array())
     {
-        
+        $this->jnotice("INVALIDURL","Invalid request");
+        /*
         require_once 'Pman/Core/Auth/Login.php';
         $n = new Pman_Core_Auth_Login();
         $n->post($base, $o);
-        
+        */
     }
     
     

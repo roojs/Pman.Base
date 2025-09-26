55a9f1f12dedab33c984ebe192c2ec77b6fcf65f
leon <leon@roojs.com>
2025-09-26 12:44:03 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -1024,7 +1024,6 @@ class Pman_Roo extends Pman
                 $_SESSION[get_class($this)]['form_uids'] = array_slice($_SESSION[get_class($this)]['form_uids'], -100);
             }
         }
-        die('test');
         
         if ($ev) { 
             $ev->audit($x);

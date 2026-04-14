451bd5eb68fa8a95a3335e79a0ddeb11fa166a11
leon <leon@roojs.com>
2026-04-14 15:41:28 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -281,7 +281,7 @@ class Pman_Roo extends Pman
 
         
         
-        if(!empty($_REQUEST['_count_join_excludee'])) {
+        if(!empty($_REQUEST['_count_join_exclude'])) {
             $xx->_join = '';
             $xx->autoJoin(array('exclude' => explode(',', $_REQUEST['_count_join_exclude'])));
             // add extra join required in the count query if any

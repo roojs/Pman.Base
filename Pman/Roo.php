e472a06b71459d479a22b37292e7e77b77123eec
leon <leon@roojs.com>
2026-04-14 13:24:59 +0800

    Pman/Roo.php

--- Pman/Roo.php
+++ Pman/Roo.php
@@ -283,6 +283,7 @@ class Pman_Roo extends Pman
         
         if(!empty($_REQUEST['_count_join_excludee']) && implode('', $xx->_join_parts) == $xx->_join) {
             $excludeTables = explode(',', $_REQUEST['_count_join_exclude']);
+            // $xx->_join_count
             $xx->_join = '';
             // $xx->_join_parts = array_filter($xx->_join_parts, function($join) use ($excludeTables) {
             //     foreach($excludeTables as $excludeTable) {
